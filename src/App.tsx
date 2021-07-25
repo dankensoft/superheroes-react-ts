914a87c6715cc3cbe6bc7ed2e89286b0e44f0d86
Danniels Castillo <danniels.magana@gmail.com>
2021-07-25 12:18:58 -0600

    Preloader Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Preloader} from "./components/preloader/Preloader";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Superheroes App - Startrack Test
-        </p>
-      </header>
-    </div>
+    <>
+      <Preloader />
+    </>
   );
 }
 
